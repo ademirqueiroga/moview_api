d21a881e057a1f05cdb0ae45e8e403eada5414af
Ademir Queiroga <ademirqueiroganeto@hotmail.com>
2017-04-09 21:19:06 -0300

    datetime format

--- moview_api/settings.py
+++ moview_api/settings.py
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': "%s000"
 }
 
 TEMPLATES = [
