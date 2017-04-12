aa6ed7a4cd5c0193115e894f410a77dc2cd51578
Ademir Queiroga <ademirqueiroganeto@hotmail.com>
2017-04-12 00:42:41 -0300

    bug fix

--- movies/urls.py
+++ movies/urls.py
@@ -6,10 +6,10 @@ from movies.views import *
 
 router = DefaultRouter()
 router.register(r'^categories', CategoryViewSet)
-router.register(r'', MovieViewSet) # NEED TO BE THE LAST ROUTE
+router.register(r'', MovieViewSet) #TEMPORARY NEED TO BE THE LAST ROUTE
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^comments/$', CommentView.as_view()),
     url(r'^details/[0-9]+/$', MovieDetailsView.as_view()),
+    url(r'^', include(router.urls)), #TEMPORARY NEED TO BE THE LAST URL
 ]
