c2530872e5ba8e1581ecaf5e771dd0ce333b50e0
Ademir Queiroga <ademirqueiroganeto@hotmail.com>
2017-05-23 18:22:26 -0300

    add Comment to admin site

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from models import Movie, Category
+from models import Movie, Category, Comment
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Category)
+admin.site.register(Comment)
