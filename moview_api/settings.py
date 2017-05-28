34eb38bd9773609a29b47bfdd8d672b4df77add9
Ademir Queiroga <ademirqueiroganeto@hotmail.com>
2017-05-28 10:08:36 -0300

    removed pagination

--- moview_api/settings.py
+++ moview_api/settings.py
@@ -66,8 +66,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
     'DATETIME_FORMAT': "%s000"
 }
 
