c2b243e6336c29cebc35c0f88cee68e649217e1e
Ademir Queiroga <ademirqueiroganeto@hotmail.com>
2017-05-28 10:22:30 -0300

    fix

--- moview_api/settings.py
+++ moview_api/settings.py
@@ -66,6 +66,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50,
     'DATETIME_FORMAT': "%s000"
 }
 
